7b994d8ee3ff694ff3a5416a0778d9df053d92da
Crowdbotics <team@crowdbotics.com>
2022-12-29 18:32:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mental_health_templ_38067",
+  url: "https://mental_health_templ_38067.botics.co",
+  api: "https://mental_health_templ_38067.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
